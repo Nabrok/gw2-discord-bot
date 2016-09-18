bf9a10033876bc6dbf9c20732e5f7857865a84ce
Nabrok.9023 <jamesorr7@gmail.com>
2016-09-18 22:35:21 +0000

    Add progression feature
    Adds the following commands:
    * !fractal level
    * !wvw rank

---
+++ features/progression.js
@@ -0,0 +1,34 @@
+var
+	async = require('async'),
+	db = require('../lib/db'),
+	phrases = require('../lib/phrases'),
+	gw2 = require('../lib/gw2_api')
+;
+
+function messageReceived(message) {
+	if (! message.content.match(/^!(fractal level|wvw rank)$/)) return;
+	async.waterfall([
+		function(next) { message.channel.startTyping(next); },
+		function(something, next) { db.getUserKey(message.author.id, next) },
+		function(key, next) { if (! key) return next(); gw2.request('/v2/account', key, next) },
+	], function(err, result) {
+		message.channel.stopTyping(function() {
+			if (! result) {
+				message.reply(phrases.get("PROGRESSION_NO_KEY"));
+				return;
+			}
+			switch (message.content) {
+				case "!fractal level":
+					message.reply(phrases.get("PROGRESSION_FRACTAL_LEVEL", { level: result.fractal_level }));
+					break;
+				case "!wvw rank":
+					message.reply(phrases.get("PROGRESSION_WVW_RANK", { rank: result.wvw_rank }));
+					break;
+			}
+		});
+	});
+}
+
+module.exports = function(bot) {
+	bot.on("message", messageReceived);
+}
