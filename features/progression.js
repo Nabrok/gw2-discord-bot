1f73ed4943363d1342ccc45be912f483f3179a5f
Nabrok.9023 <jamesorr7@gmail.com>
2016-09-21 16:56:00 +0000

    Warn if insuffient scope on API key
    This closes #4.

--- features/progression.js
+++ features/progression.js
@@ -9,12 +9,23 @@ function messageReceived(message) {
 	if (! message.content.match(/^!(fractal level|wvw rank)$/)) return;
 	async.waterfall([
 		function(next) { message.channel.startTyping(next); },
-		function(something, next) { db.getUserKey(message.author.id, next) },
+		function(something, next) { db.checkKeyPermission(message.author.id, 'progression', next) },
+		function(hasPerm, next) {
+			if (! hasPerm) next(new Error("requires scope progression"));
+			else next();
+		},
+		function(next) { db.getUserKey(message.author.id, next) },
 		function(key, next) { if (! key) return next(); gw2.request('/v2/account', key, next) },
 	], function(err, result) {
 		message.channel.stopTyping(function() {
+			if (err) {
+				if (err.message === "endpoint requires authentication") message.reply(phrases.get("CORE_NO_KEY"));
+				if (err.message === "requires scope progression") message.reply(phrases.get("CORE_MISSING_SCOPE", { scope: 'progression' }));
+				else console.log(err.message);
+				return;
+			}
 			if (! result) {
-				message.reply(phrases.get("PROGRESSION_NO_KEY"));
+				message.reply(phrases.get("CORE_NO_KEY"));
 				return;
 			}
 			switch (message.content) {
