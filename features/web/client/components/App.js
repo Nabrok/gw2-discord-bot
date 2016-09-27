488f3bf639a63ea887af1173f34ab4f639347259
Nabrok.9023 <jamesorr7@gmail.com>
2016-09-27 19:01:12 +0000

    Add bootstrap to improve look.

--- features/web/client/components/App.js
+++ features/web/client/components/App.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import { Navbar, Nav, NavItem, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import LoginStore from '../stores/LoginStore';
 
 export default class App extends React.Component {
+	constructor() {
+		super();
+		this.state = this._getLoginState();
+	}
+
+	componentDidMount() {
+		this.changeListener = this._onChange.bind(this);
+		LoginStore.addChangeListener(this.changeListener);
+	}
+
+	componentWillUnmount() {
+		LoginStore.removeChangeListener(this.changeListener);
+	}
+
+	_onChange() {
+		this.setState(this._getLoginState());
+	}
+
+	_getLoginState() {
+		return {
+			userLoggedIn: LoginStore.isLoggedIn(),
+			user: LoginStore.username
+		};
+	}
+
 	render() {
 		return (
 			<div>
-				<h1>Discord Guild Wars 2 Bot</h1>
-				<div>{this.props.children}</div>
+				<Navbar>
+					<Navbar.Header><Navbar.Brand><Link to="/">Discord Guild Wars 2 Bot</Link></Navbar.Brand></Navbar.Header>
+					<Nav pullRight>
+						{ this.state.userLoggedIn && <NavDropdown eventKey={1} title={ this.state.user } id="user_dropdown">
+							<LinkContainer to="/logout"><NavItem eventKey={1}>Logout</NavItem></LinkContainer>
+						</NavDropdown> }
+					</Nav>
+				</Navbar>
+				<div className="container">{this.props.children}</div>
 			</div>
 		);
 	}
--- features/web/client/components/Login.js
+++ features/web/client/components/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Well, Button } from 'react-bootstrap';
 
 export default class Login extends React.Component {
 	render() {
 		return (
 			<div>
-				<a href="/login">Login</a>
+				<Well bsSize="large">The button below will send you to the discord site to enter your credentials.  You will then be redirected back to this site.</Well>
+				<Button bsStyle="primary" bsSize="large" block href="/login">Login</Button>
 			</div>
 		);
 	}
