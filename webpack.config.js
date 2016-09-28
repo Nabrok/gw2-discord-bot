1301a553d22152848ab512b0d694def04addad7f
Nabrok.9023 <jamesorr7@gmail.com>
2016-09-28 04:33:31 +0000

    Split vendor packages into separate bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 var webpack = require('webpack');
 
 module.exports = {
-	entry: './features/web/client/index.js',
+	entry: {
+		app: './features/web/client/index.js',
+		vendor: ['react', 'react-dom', 'flux', 'react-bootstrap', 'react-router', 'react-router-bootstrap', 'socket.io-client', 'superagent', 'jwt-decode']
+	},
 	output: {
 		path: './features/web/public',
 		filename: 'bundle.js',
 		publicPath: ''
 	},
 	plugins: process.env.NODE_ENV === 'production' ? [
+		new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
 		new webpack.ExtendedAPIPlugin(),
 		new webpack.DefinePlugin({
 			'process.env': { 'NODE_ENV': JSON.stringify('production') }
@@ -16,6 +20,7 @@ module.exports = {
 		new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.optimize.UglifyJsPlugin()
 	] : [
+		new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
 		new webpack.ExtendedAPIPlugin()
 	],
 	module: {
