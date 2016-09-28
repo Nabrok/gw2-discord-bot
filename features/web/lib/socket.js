cce48d9d3ec25066a2304b6deb3f407a3b4c2b42
Nabrok.9023 <jamesorr7@gmail.com>
2016-09-28 20:12:00 +0000

    Some more promise changes in socket

--- features/web/lib/socket.js
+++ features/web/lib/socket.js
@@ -2,36 +2,37 @@ var
 	Promise = require('bluebird'),
 	discordAuth = require('./discord-auth'),
 	config = require('config'),
-	nJwt = require('njwt'),
+	nJwt = Promise.promisifyAll(require('njwt')),
 	db = Promise.promisifyAll(require('../../../lib/db')),
 	gw2 = Promise.promisifyAll(require('../../../lib/gw2'))
 ;
 
 var jwt_secret = config.has('web.jwt_secret') ? config.get('web.jwt_secret') : config.get('discord.client_secret');
 
+function verifyJwt(data) {
+	return nJwt.verifyAsync(data.jwt, jwt_secret).then(verifiedJwt => {
+		data.jwt = verifiedJwt;
+		data.user = JSON.parse(verifiedJwt.body.sub);
+		return data;
+	}).catch(e => {
+		throw new Error('Invalid token');
+	});
+}
+
 function newConnection(socket) {
 	socket.on('Login', discordAuth.processLogin);
 
-	socket.verifiedOn = (e, callback) => {
-		socket.on(e, (payload, cb) => {
-			nJwt.verify(payload.jwt, jwt_secret, (err, verifiedJwt) => {
-				if (err) return cb({ error: 'Invalid token' });
-				payload.jwt = verifiedJwt;
-				payload.user = JSON.parse(verifiedJwt.body.sub);
-				callback(payload, cb);
-			});
-		});
-	};
-
-	socket.verifiedOn('get token', (data, cb) => {
-		db.getUserTokenAsync(data.user.id)
+	socket.on('get token', (data, cb) => {
+		verifyJwt(data)
+			.then(() => db.getUserTokenAsync(data.user.id))
 			.then(token => cb({ message: 'success', data: JSON.parse(token) }))
 			.catch(err => cb({ error: err.message }))
 		;
 	});
 
-	socket.verifiedOn('set key', (data, cb) => {
-		gw2.requestAsync('/v2/tokeninfo', data.data)
+	socket.on('set key', (data, cb) => {
+		verifyJwt(data)
+			.then(() => gw2.requestAsync('/v2/tokeninfo', data.data))
 			.then(token => db.setUserKeyAsync(data.user.id, data.data, token))
 			.then(() => cb({ message: 'success' }))
 			.catch(err => cb({ error: err.message }))
