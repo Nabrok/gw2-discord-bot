91deaccb08fbbb63fec42f9ea8045c00a68d2b88
Archomeda <archomeda@hotmail.com>
2016-10-08 07:12:13 +0200

    Improve !whois

--- features/whois.js
+++ features/whois.js
@@ -1,45 +1,52 @@
-var
-	async = require('async'),
-	db = require('../lib/db'),
-	phrases = require('../lib/phrases')
-;
+"use strict";
+const
+	Promise = require("bluebird"),
+	db = Promise.promisifyAll(require("../lib/db")),
+	phrases = require("../lib/phrases");
 
-var bot_user;
+let bot_user;
 
 function messageReceived(message) {
-	if (message.content.match(new RegExp('^!'+phrases.get("WHOIS_WHOIS")+' (.*)+$', 'i'))) {
-		if (message.mentions.length === 0) return;
-		var user = message.mentions[0];
-		async.waterfall([
-			function(next) { message.channel.startTyping(next); },
-			function(something, next) {
-				if (user.id === bot_user.id) return next('bot user');
-				db.getAccountByUser(user.id, next);
-			},
-			function(account, next) { account ? next(null, account.name) : next('unknown'); }
-		], function(err, result) {
-			message.channel.stopTyping(function() {
-				if (err) {
-					switch (err) {
-						case 'unknown':
-							return message.reply(phrases.get("WHOIS_UNKNOWN"));
-						case 'bot user':
-							return message.reply(phrases.get("WHOIS_BOT", { user: bot_user.mention() }));
-						default:
-							return console.log(err.message);
-					}
-				} else if (result) {
-					if (user.id === message.author.id) message.reply(phrases.get("WHOIS_SELF", { account_name: result }));
-					else message.reply(phrases.get("WHOIS_KNOWN", { user: user.mention(), account_name: result }));
-				} else {
-					message.reply(phrases.get("WHOIS_UNKNOWN"));
+	const messageAsync = Promise.promisifyAll(message);
+	const channelAsync = Promise.promisifyAll(message.channel);
+
+	if (message.content.match(new RegExp(`^!${phrases.get("WHOIS_WHOIS")} (.*)?$`, "i"))) {
+		if (message.mentions.length === 0) return; // No mentions? No answer
+		const user = message.mentions[0];
+		channelAsync.startTypingAsync()
+			.then(() => {
+				if (user.id === bot_user.id) throw new Error("bot user");
+
+				// Get the GW2 account data by user
+				return db.getAccountByUserAsync(user.id);
+			})
+			.then(account => {
+				if (!account) throw new Error("unknown user");
+
+				// Construct message
+				if (user.id === message.author.id) return phrases.get("WHOIS_SELF", { account_name: account.name });
+				else return phrases.get("WHOIS_KNOWN", { user: user.mention(), account_name: account.name });
+			})
+			.catch(err => {
+				// Capture errors and construct proper fail message
+				switch (err.message) {
+					case "bot user":
+						return phrases.get("WHOIS_BOT", { user: bot_user.mention() });
+					case "unknown user":
+						return phrases.get("WHOIS_UNKNOWN");
+					default:
+						console.log(`Error in whois command initiated by ${message.author.username}#${message.author.discriminator}: ${err.message}`);
+						return;
 				}
-			});
-		});
+			})
+			.finally(() => channelAsync.stopTypingAsync())
+			.then(text => messageAsync.replyAsync(text));
 	}
 }
 
-module.exports = function(bot) {
+module.exports = bot => {
 	bot.on("message", messageReceived);
-	bot.on("ready", function() { bot_user = bot.user; })
+	bot.on("ready", () => {
+		bot_user = bot.user;
+	})
 };
