830118b86e2c49166b34d4adb3218f72799086a2
Nabrok.9023 <jamesorr7@gmail.com>
2016-12-23 14:54:18 -0500

    Add Legendary Insight count

---
+++ features/li.js
@@ -0,0 +1,65 @@
+var
+	Promise = require('bluebird'),
+	db = Promise.promisifyAll(require('../lib/db')),
+	gw2 = require('../lib/gw2'),
+	phrases = require('../lib/phrases')
+;
+
+const li_id = 77302; // ID number for Legendary Insights
+
+function countLI(user) {
+	return Promise.all([db.getUserKeyAsync(user.id), db.getUserTokenAsync(user.id)])
+	.then(result => {
+		var key = result[0], token = result[1] ? JSON.parse(result[1]) : {};
+		if (! key) throw new Error("endpoint requires authentication");
+		var permissions = token.permissions || [];
+		if (permissions.indexOf('inventories') === -1) throw new Error('inventories permission required');
+		if (permissions.indexOf('characters') === -1) throw new Error('characters permission required');
+		var queries = [];
+		queries.push({ name: 'bank', promise: () => gw2.request('/v2/account/bank', key) });
+		queries.push({ name: 'inventory', promise: () => gw2.request('/v2/account/inventory', key) });
+		queries.push({ name: 'characters', promise: () => gw2.request('/v2/characters', key).then(characters => {
+			var char_queries = characters.map(c => ({
+				name: c,
+				promise: () => gw2.request('/v2/characters/'+encodeURIComponent(c)+'/inventory', key)
+			}));
+			return Promise.map(char_queries, c => c.promise(), { concurrency: 3 });
+		})});
+		return Promise.map(queries, q => q.promise());
+	})
+	.then(result => {
+		var bank = result[0], shared = result[1], characters = result[2];
+		var bank_insights = bank.filter(item => !!item && item.id === li_id).reduce((total, item) => { total += item.count; return total; }, 0);
+		var shared_insights = shared.filter(item => !!item && item.id === li_id).reduce((total, item) => { total += item.count; return total; }, 0);
+		var char_insights = characters.reduce((total, character) =>
+			character.bags.filter(b => !!b).reduce((char_total, bag) =>
+				bag.inventory.filter(i => !!i && i.id === li_id).reduce((bag_total, item) =>
+					{ bag_total += item.count; return bag_total; }
+				, char_total)
+			, total)
+		, 0);
+		return bank_insights + shared_insights + char_insights;
+	});
+}
+
+function messageReceived(message) {
+	var cmd = new RegExp('^!'+phrases.get("LI_CMD")+'$', 'i');
+	if (! message.content.match(cmd)) return;
+	var messageAsync = Promise.promisifyAll(message);
+	var channelAsync = Promise.promisifyAll(message.channel);
+	channelAsync.startTyping()
+	.then(() => countLI(message.author))
+	.then(count => messageAsync.reply(phrases.get("LI_SHOW", { count })))
+	.catch(err => {
+		if (err.message === "endpoint requires authentication") return messageAsync.reply(phrases.get("CORE_NO_KEY"));
+		if (err.message === "inventories permissions required") return messageAsync.reply(phrases.get("CORE_MISSING_SCOPE", { scope: 'inventories' }));
+		if (err.message === "characters permissions required") return messageAsync.reply(phrases.get("CORE_MISSING_SCOPE", { scope: 'characters' }));
+		console.error(err.stack);
+		return messageAsync.reply(phrases.get("CORE_ERROR"));
+	})
+	.then(() => channelAsync.stopTyping());
+}
+
+module.exports = function(bot) {
+	bot.on("message", messageReceived);
+}
