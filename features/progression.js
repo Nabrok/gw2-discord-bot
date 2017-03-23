5d56439bd7df07bdbb0d40d691c020d042e93012
Nabrok.9023 <jamesorr7@gmail.com>
2017-03-22 21:47:52 -0400

    Update progression to use new discord.js

--- features/progression.js
+++ features/progression.js
@@ -1,6 +1,6 @@
 var
-	async = require('async'),
-	db = require('../lib/db'),
+	Promise = require('bluebird'),
+	db = Promise.promisifyAll(require('../lib/db')),
 	phrases = require('../lib/phrases'),
 	gw2 = require('../lib/gw2')
 ;
@@ -9,45 +9,30 @@ function messageReceived(message) {
 	var fractal_cmd = phrases.get("PROGRESSION_FRACTAL");
 	var wvw_cmd = phrases.get("PROGRESSION_WVW");
 	if (! message.content.match(new RegExp('^!('+fractal_cmd+'|'+wvw_cmd+')$', 'i'))) return;
-	async.waterfall([
-		function(next) { message.channel.startTyping(next); },
-		function(something, next) { db.checkKeyPermission(message.author.id, 'progression', next) },
-		function(hasPerm, next) {
-			if (! hasPerm) next(new Error("requires scope progression"));
-			else next();
-		},
-		function(next) { db.getUserKey(message.author.id, next) },
-		function(key, next) { if (! key) return next(); gw2.request('/v2/account', key, next) },
-		function(result, next) {
+	message.channel.startTyping();
+	db.checkKeyPermissionAsync(message.author.id, 'progression')
+		.then(hasPerm => {
+			if (! hasPerm) throw new Error("requires scope progression");
+			return db.getUserKeyAsync(message.author.id);
+		})
+		.then(key => {
+			if (! key) throw new Error("endpoint requires authentication");
+			return gw2.request('/v2/account', key);
+		})
+		.then(account => {
 			if (message.content.match(new RegExp('^!'+wvw_cmd+'$', 'i')))
-				gw2.getWvwRankName(result.wvw_rank).then(rank_name => {
-					result.wvw_rank_name = rank_name;
-					next(null, result);
-				});
-			else
-				next(null, result);
-		}
-	], function(err, result) {
-		message.channel.stopTyping(function() {
-			if (err) {
-				if (err.message === "endpoint requires authentication") message.reply(phrases.get("CORE_NO_KEY"));
-				if (err.message === "requires scope progression") message.reply(phrases.get("CORE_MISSING_SCOPE", { scope: 'progression' }));
-				else {
-					message.reply(phrases.get("CORE_ERROR"));
-					console.log(err.message);
-				}
-				return;
-			}
-			if (! result) {
-				message.reply(phrases.get("CORE_NO_KEY"));
-				return;
-			}
-			if (message.content.match(new RegExp('^!'+fractal_cmd+'$', 'i')))
-					message.reply(phrases.get("PROGRESSION_FRACTAL_LEVEL", { level: result.fractal_level }));
-			else if (message.content.match(new RegExp('^!'+wvw_cmd+'$', 'i')))
-					message.reply(phrases.get("PROGRESSION_WVW_RANK", { rank: result.wvw_rank, title: result.wvw_rank_name }));
-		});
-	});
+				return gw2.getWvwRankName(account.wvw_rank)
+					.then(rank_name => message.reply(phrases.get("PROGRESSION_WVW_RANK", { rank: account.wvw_rank, title: rank_name })));
+			else if (message.content.match(new RegExp('^!'+fractal_cmd+'$', 'i')))
+				return message.reply(phrases.get("PROGRESSION_FRACTAL_LEVEL", { level: account.fractal_level }));
+		})
+		.catch(err => {
+			if (err.message === "endpoint requires authentication") return message.reply(phrases.get("CORE_NO_KEY"));
+			if (err.message === "requires scope progression") return message.reply(phrases.get("CORE_MISSING_SCORE", { scope: 'progression' }));
+			console.error(e.stack)
+		})
+		.then(() => message.channel.stopTyping())
+	;
 }
 
 module.exports = function(bot) {
