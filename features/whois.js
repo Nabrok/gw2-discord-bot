d41ace4544240f255f414c10170e12a690a38563
Nabrok <jamesorr7@gmail.com>
2019-10-11 15:58:18 -0400

    Fix whois bot param

--- features/whois.js
+++ features/whois.js
@@ -39,7 +39,7 @@ async function messageReceived(message) {
 }
 
 /**
- * @param {import('discord.js')} bot
+ * @param {import('discord.js').Client} bot
  */
 module.exports = bot => {
 	bot.on("message", messageReceived);
